91b7ffbefc789b79e8128da576130ea65c6a4598
Elaine Tan <elainet@comp.nus.edu.sg>
2020-05-24 16:22:39 +0800

    Updates

---
+++ precache-manifest.0238f47c85e7ff0301c74b8dcd12962b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c7111ee4e956e7a452d1b55527bb87",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "016474c21431551385d6",
+    "url": "/moviedb/static/css/main.a6286d00.chunk.css"
+  },
+  {
+    "revision": "6ac14f86dd76543400f0",
+    "url": "/moviedb/static/js/2.7b0d4e6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb/static/js/2.7b0d4e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016474c21431551385d6",
+    "url": "/moviedb/static/js/main.0ee75f08.chunk.js"
+  },
+  {
+    "revision": "3ef2c0b5513e19a297e2",
+    "url": "/moviedb/static/js/runtime-main.3878a2e8.js"
+  }
+]);
